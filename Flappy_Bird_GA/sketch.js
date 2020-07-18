ea9602053fbafb1f07e343f7e6b0562d61dec4ed
Axellben <cretu.alexandru2000@gmail.com>
2020-07-18 17:54:03 +0300

    Improve UI

--- Flappy_Bird_GA/sketch.js
+++ Flappy_Bird_GA/sketch.js
@@ -1,17 +1,20 @@
 const TOTAL = 500;
+const pipeInterval = 50;
 let birds = [];
 let saveBirds = [];
 let pipes = [];
 let counter = 0;
 let slider;
 let bgX = 0;
+let score = 0;
 let parallax = 0.1,
   speed;
-let birdImg, pipeImg, pipeImgR, groundImg, bgImg;
+let birdImg, pipeImg, pipeImgR, groundImg, bgImg, flappyFont;
 let checkbox;
 let showBestBird = false;
 
 function preload() {
+  flappyFont = loadFont("assets/04B_19__.TTF");
   birdImg = loadImage("assets/yellowbird-midflap.png");
   pipeImg = loadImage("assets/flappy_pipe.png");
   pipeImgR = loadImage("assets/flappy_pipe_r.png");
@@ -21,9 +24,8 @@ function preload() {
 
 function setup() {
   createCanvas(bgImg.width, bgImg.height + groundImg.height);
+  createP("Fast forward");
   slider = createSlider(1, 100, 1);
-  checkbox = createCheckbox("ShowBestBird", false);
-  checkbox.changed(myCheckedEvent);
   for (i = 0; i < TOTAL; i++) {
     birds[i] = new Bird();
   }
@@ -31,7 +33,7 @@ function setup() {
 
 function draw() {
   for (let n = 0; n < slider.value(); n++) {
-    if (counter % 50 == 0) {
+    if (counter % pipeInterval == 0) {
       pipes.push(new Pipe());
       speed = pipes[0].speed;
     }
@@ -54,12 +56,15 @@ function draw() {
     for (let bird of birds) {
       bird.think(pipes);
       bird.update();
+      score = max(score, bird.score);
     }
+    score = floor(score / pipeInterval);
 
     if (birds.length === 0) {
       counter = 0;
       nextGeneration();
       pipes = [];
+      score = 0;
     }
 
     for (let i = birds.length - 1; i >= 0; i--) {
@@ -72,6 +77,7 @@ function draw() {
   //All the drawing
 
   // Draw BackGround and create a paralex effect
+
   image(bgImg, bgX, 0, bgImg.width, height);
   bgX -= speed * parallax;
 
@@ -82,12 +88,8 @@ function draw() {
     }
   }
 
-  if (showBestBird == false) {
-    for (let bird of birds) {
-      bird.show();
-    }
-  } else {
-    birds[0].show();
+  for (let bird of birds) {
+    bird.show();
   }
 
   for (let pipe of pipes) {
@@ -102,19 +104,21 @@ function draw() {
     groundImg.width,
     groundImg.height
   );
+
+  // For scores
+  textFont(flappyFont);
+  textSize(28);
+  textAlign(CENTER);
+  stroke(0);
+  strokeWeight(2);
+  fill(0);
+  text(score, width / 2 + 1, height / 10 + 1);
+  fill(255);
+  text(score, width / 2, height / 10);
 }
 
-function keyPressed() {
-  if (key == " ") {
-    bird.up();
-  }
-}
-
-function myCheckedEvent() {
-  if (this.checked()) {
-    showBestBird = true;
-  } else {
-    showBestBird = false;
-  }
-  nextGeneration();
-}
+// function keyPressed() {
+//   if (key == " ") {
+//     bird.up();
+//   }
+// }
